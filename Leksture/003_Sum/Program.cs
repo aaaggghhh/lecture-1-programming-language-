ee5d0288fd664d20c27e20177a42f9bab3ce6f40
Aleksey <shatalov_alekse@mail.ru>
2022-10-17 21:57:10 +0700

    деление двух чисел с остатком

--- Leksture/003_Sum/Program.cs
+++ Leksture/003_Sum/Program.cs
@@ -1,7 +1,7 @@
 ﻿// сплюсовать 2 числа и вывести на экран
 
-int numberA = 3;
-int numberB = 9;
-int result = numberA + numberB;
+double numberA = 36;
+double numberB = 9;
+double result = numberA + numberB;
 Console.WriteLine (result);
 
---
+++ Leksture/004_Div/Program.cs
@@ -0,0 +1,5 @@
+﻿//деление двух чисел с остатком
+double numberA = 30;
+double numberB = 9;
+
+Console.WriteLine (numberA / numberB);
